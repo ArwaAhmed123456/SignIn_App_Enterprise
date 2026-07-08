aeb4d710b1b0d762cc51e449f1dd78289073745d
Miracle <miracle@example.com>
2026-07-08 12:47:25 +0500

    fix(mobile): correct nativewind v4 setup, fix babel/metro config, add global.css

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,13 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
+const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-// Fix for react-native-web resolution errors in newer Expo/RN versions
-config.resolver.alias = {
-    ...config.resolver.alias,
-    'react-native-web/dist/exports/ActivityIndicator': 'react-native-web/dist/exports/ActivityIndicator/index.js',
-    'react-native-web/dist/exports/DeviceEventEmitter': 'react-native-web/dist/exports/DeviceEventEmitter/index.js'
-};
-
-module.exports = config;
+module.exports = withNativeWind(config, { input: './global.css' });
