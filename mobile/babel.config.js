52575fd801f595ea4434f7de8bb5345b3410171f
Miracle <miracle@example.com>
2026-07-14 16:27:52 +0500

    Fix NativeWind Babel configuration

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,7 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }]],
-    plugins: ['nativewind/babel', 'react-native-reanimated/plugin'],
+    // NativeWind v4 (Expo) must be added as a PRESET, not a plugin.
+    // Putting it in plugins causes: ".plugins is not a valid Plugin property"
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
+    ],
+    // Reanimated plugin must be listed (and should be last)
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
